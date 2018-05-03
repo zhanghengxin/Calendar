c72e670c7a57b4737cdcb572b15fffca187f98bf
hkk <812718649@qq.com>
2018-05-03 14:56:34 +0800

    更新兼容IE

--- Calendar.js
+++ Calendar.js
@@ -1,4 +1,5 @@
-;(function(undefined) {
+;
+(function (undefined) {
     var _global;
     //工具函数
     //配置合并
@@ -17,11 +18,11 @@
         d = (d.toString())[1] ? d : '0' + d;
         return y + symbol + m + symbol + d
     }
-
+    ////debugger;
     function Schedule(opt) {
         var def = {},
             opt = extend(def, opt, true),
-            curDate = opt.date ? new Date(opt.date) : new Date(),
+            curDate = opt.date ? new Date(opt.date.replace(new RegExp(/-/g), "/")) : new Date(),
             year = curDate.getFullYear(),
             month = curDate.getMonth(),
             day = curDate.getDate(),
@@ -32,58 +33,65 @@
             externalEl = document.querySelector(opt.externalEl) || document.querySelector('body'),
             el = document.querySelector(opt.el) || document.querySelector('body'),
             _this = this;
-        var bindEvent = function() {
-            //一星期的点击事件
-            externalEl.addEventListener('click', function (e) {
-                debugger;
-                switch (e.target.id) {
-                    case 'nextWeek':
-                        _this.nextWeekFun();
-                        opt.clickCb && opt.clickCb(year, month + 1, day);
-                        //render()
-                        break;
-                    case 'prevWeek':
-                        _this.prevWeekFun();
-                        opt.clickCb && opt.clickCb(year, month + 1, day);
-                        //render()
-                        break;
-                    default:
-                        break;
-                };
-            }, false);
-            el.addEventListener('click', function(e) {
-                debugger;
-                switch (e.target.id) {
-                    case 'nextWeek':
-                        _this.nextWeekFun();
-                        break;
-                    case 'nextMonth':
-                        _this.nextMonthFun();
-                        break;
-                    case 'nextYear':
-                        _this.nextYearFun();
-                        break;
-                    case 'prevWeek':
-                        _this.prevWeekFun();
-                        break;
-                    case 'prevMonth':
-                        _this.prevMonthFun();
-                        break;
-                    case 'prevYear':
-                        _this.prevYearFun();
-                        break;
-                    default:
-                        break;
-                };
-                if (e.target.className.indexOf('currentDate') > -1) {
-                    opt.clickCb && opt.clickCb(year, month + 1, e.target.innerHTML);
-                    selectedDate = e.target.title;
-                    day = e.target.innerHTML;
-                    render();
-                }
-            }, false)
+        var selectOperation = function (e) {
+            //IE8 event兼容
+            var event = e || window.event;
+            var target = event.target || event.srcElement;
+            switch (target.id) {
+                case 'nextWeek':
+                    _this.nextWeekFun();
+                    opt.clickCb && opt.clickCb(year, month + 1, day);
+                    //selectedDate = e.target.title;
+                    render()
+                    break;
+                case 'prevWeek':
+                    _this.prevWeekFun();
+                    opt.clickCb && opt.clickCb(year, month + 1, day);
+                    //selectedDate = e.target.title;
+                    render()
+                    break;
+                case 'nextWeek':
+                    _this.nextWeekFun();
+                    break;
+                case 'nextMonth':
+                    _this.nextMonthFun();
+                    break;
+                case 'nextYear':
+                    _this.nextYearFun();
+                    break;
+                case 'prevWeek':
+                    _this.prevWeekFun();
+                    break;
+                case 'prevMonth':
+                    _this.prevMonthFun();
+                    break;
+                case 'prevYear':
+                    _this.prevYearFun();
+                    break;
+                default:
+                    break;
+            };
+            if (target.className.indexOf('currentDate') > -1) {
+                opt.clickCb && opt.clickCb(year, month + 1, target.innerHTML);
+                selectedDate = target.title;
+                day = target.innerHTML;
+                render();
+            }
         }
-        var init = function() {
+        var bindEvent = function () {
+            //debugger;
+            if (externalEl.addEventListener || el.addEventListener) {
+                externalEl && externalEl.removeEventListener('click', selectOperation, false);
+                externalEl.addEventListener('click', selectOperation, false);
+                //!externalEl && (el && el.removeEventListener('click', selectOperation, false), el.addEventListener('click', selectOperation, false));
+            } else if (externalEl.attachEvent || el.attachEvent) {
+                externalEl && externalEl.detachEvent('onclick', selectOperation);
+                externalEl.attachEvent('onclick', selectOperation);
+                //!externalEl && (el && el.detachEvent('onclick', selectOperation), el.attachEvent('onclick', selectOperation));
+            }
+        }
+        var init = function () {
+            //debugger;
             var scheduleHd = '<div class="schedule-hd">' +
                 '<div>' +
                 '<span class="arrow icon iconfont icon-116leftarrowheads" id="prevYear" ></span>' +
@@ -109,8 +117,7 @@
             bindEvent();
             render();
         }
-        var render = function() {
-            debugger;
+        var render = function () {
             var fullDay = new Date(year, month + 1, 0).getDate(), //当月总天数
                 startWeek = new Date(year, month, 1).getDay(), //当月第一天是周几
                 total = (fullDay + startWeek) % 7 == 0 ? (fullDay + startWeek) : fullDay + startWeek + (7 - (fullDay + startWeek) % 7), //元素总个数
@@ -132,26 +139,28 @@
             el.querySelector('.schedule-bd').innerHTML = eleTemp.join('');
             el.querySelector('.today').innerHTML = formartDate(year, month + 1, day, '-');
             //7天时间计算
-            var today = formartDate(year, month + 1, day, '-');
+            var today = formartDate(year, month + 1, day, '/');
+            ////debugger;
+            var date1 = formartDate(year, month + 1, day, '-');
             var date2 = new Date(today);
             date2.setDate(date2.getDate() + 6);
             date2 = date2.getFullYear() + "-" + (date2.getMonth() + 1) + "-" + date2.getDate();
-            $(".hzqx-main-time-select").html(today + "至" + date2);
+            $(".hzqx-main-time-select").html(date1 + "至" + date2);
 
         };
-        this.nextWeekFun = function() {
-            debugger;
-            var today = formartDate(year, month + 1, day, '-');
-            var nextWeek = new Date(today) - 0 + (7*24*60*60*1000);
-            //nextWeek.setDate(nextWeek.getDate() + 7);
-            nextWeek = new Date(nextWeek)
+        this.nextWeekFun = function () {
+            
+            //var today = new Date(year, month, day);
+            //var nextWeek = today.setDate(today.getDate() + 7);
+            var today = new Date(year, month, day) - 0;
+            var nextWeek = new Date(today - 0 + 7 * 24 * 60 * 60 * 1000);
             year = nextWeek.getFullYear();
             month = nextWeek.getMonth();
             day = nextWeek.getDate();
             render();
             opt.nextWeekCb && opt.nextWeekCb(year, month + 1, day);
         },
-        this.nextMonthFun = function() {
+            this.nextMonthFun = function () {
             if (month + 1 > 11) {
                 year += 1;
                 month = 0;
@@ -161,25 +170,23 @@
             render();
             opt.nextMonthCb && opt.nextMonthCb(year, month + 1, day);
         },
-        this.nextYearFun = function() {
+            this.nextYearFun = function () {
             year += 1;
             render();
             opt.nextYeayCb && opt.nextYeayCb(year, month + 1, day);
         },
-        this.prevWeekFun = function() {
-            debugger
-            var today = formartDate(year, month + 1, day, '-');
-            var prevWeek = new Date(today) - (7*24*60*60*1000);
-            //nextWeek = nextWeek - (7*24*60*60*1000);
-            prevWeek = new Date(prevWeek)
-            //nextWeek.setDate(nextWeek.getDate() - 7);
+            this.prevWeekFun = function () {
+            //var today = new Date(year, month, day);
+            //var prevWeek = today.setDate(today.getDate() - 7);
+            var today = new Date(year, month, day);
+            var prevWeek = new Date(today - 7 * 24 * 60 * 60 * 1000)
             year = prevWeek.getFullYear();
             month = prevWeek.getMonth();
             day = prevWeek.getDate();
             render();
             opt.prevWeekCb && opt.prevWeekCb(year, month + 1, day);
         },
-        this.prevMonthFun = function() {
+            this.prevMonthFun = function () {
             if (month - 1 < 0) {
                 year -= 1;
                 month = 11;
@@ -189,28 +196,29 @@
             render();
             opt.prevMonthCb && opt.prevMonthCb(year, month + 1, day);
         },
-        this.prevYearFun = function() {
+            this.prevYearFun = function () {
             year -= 1;
             render();
             opt.prevYearCb && opt.prevYearCb(year, month + 1, day);
         },
-        this.init = function(DATE) {
-            // body... 抛出接口，更新日历
-            year = DATE && new Date(DATE).getFullYear() //|| year;
-            month = DATE && new Date(DATE).getMonth() //|| month;
-            day = DATE && new Date(DATE).getDate() //|| day;
+            this.init2 = function (DATE) {
+            // body...
+            var DATE_STR = DATE.replace(new RegExp(/-/g), "/")
+            year = DATE && new Date(DATE_STR).getFullYear() //|| year;
+            month = DATE && new Date(DATE_STR).getMonth() //|| month;
+            day = DATE && new Date(DATE_STR).getDate() //|| day;
             render();
         }
         init();
     }
     //将插件暴露给全局对象
-    _global = (function() {
+    _global = (function () {
         return this || (0, eval)('this')
     }());
     if (typeof module !== 'undefined' && module.exports) {
         module.exports = Schedule;
     } else if (typeof define === "function" && define.amd) {
-        define(function() {
+        define(function () {
             return Schedule;
         })
     } else {
